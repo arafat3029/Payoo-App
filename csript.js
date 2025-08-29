4606182d6a2c663d6d8cfcb15f53105f50f1794c
arafat3029 <mdarafat12890@gmail.com>
2025-08-29 23:35:24 +0600

    error fixed

---
+++ csript.js
@@ -0,0 +1,6 @@
+// login button functionality
+document.getElementById("loginButton").addEventListener("click",function(e){
+    e.preventDefault()
+    console.log("button click")
+    console.log(e)
+})
